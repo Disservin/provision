6f9820a990e314ad9471f57165e75b864694bcdc
Disservin <disservin.social@gmail.com>
2024-08-13 22:09:32 +0200

    fix chezmoi

--- development/modules/chezmoi.sh
+++ development/modules/chezmoi.sh
@@ -2,9 +2,8 @@
 
 if ! command -v chezmoi &> /dev/null; then
   echo "Installing chezmoi..."
-  sh -c "$(curl -fsLS get.chezmoi.io)" -- -b /usr/local/bin
+  sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply $1
 else
   echo "chezmoi is already installed"
 fi
 
-chezmoi init --apply $1
