99cf3414df1d51db1c9027c53fb8e7d3ac33460d
Disservin <disservin.social@gmail.com>
2024-08-13 21:55:35 +0200

    fix

--- development/modules/nerd_font.sh
+++ development/modules/nerd_font.sh
@@ -2,6 +2,8 @@
 
 echo "Installing Nerd Fonts..."
 
+sudo apt install unzip fontconfig -y
+
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/JetBrainsMono.zip -O JetBrainsMono.zip
 unzip JetBrainsMono.zip -d JetBrainsMono
 sudo mv JetBrainsMono /usr/share/fonts/
